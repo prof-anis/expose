ac4d5f2b5ec42291d1fb2a0a7b6579b73ef0e0a6
Tobi Anifowose <tobi.anifowose@internetbrands.com>
2023-10-13 15:14:23 +0100

    Add autodeployer

--- deploy.php
+++ deploy.php
@@ -2,26 +2,35 @@
 
 namespace Deployer;
 
+// Include the Laravel & rsync recipes
 require 'recipe/laravel.php';
 require 'contrib/rsync.php';
 
-// Config
+define("SOURCE_ROOT", dirname(__DIR__, 2));
+define('DEPLOY_DOMAINS', ['tobexkee.com' => 'tobexkee.com']);
+define('SUPERVISOR_CONFIGS', ['expose.conf',]);
 
-set('repository', 'https://github.com/prof-anis/expose.git');
 set('application', 'Expose');
-set('ssh_multiplexing', true); // Speeds up deployments
-set('http_user', 'ubuntu');
+set('ssh_multiplexing', true); // Speed up deployment
 
 set('rsync_src', function () {
-    return __DIR__; // If your project isn't in the root, you'll need to change this.
+    return SOURCE_ROOT; // If your project isn't in the root, you'll need to change this.
 });
 
+add('shared_dirs', [
+    'config/.expose',
+]);
+
+add('writable_dirs', [
+    'config/.expose',
+]);
+
+// Configuring the rsync exclusions.
+// You'll want to exclude anything that you don't want on the production server.
 add('rsync', [
     'exclude' => [
         '.git',
         '/.env',
-        '/storage/',
-        '/config-mailcoach-app/',
         '/vendor/',
         '/node_modules/',
         '.github',
@@ -29,58 +38,91 @@ add('rsync', [
     ],
 ]);
 
-// Set up a deployer task to copy secrets to the server.
-// Since our secrets are stored in Gitlab, we can access them as env vars.
-task('deploy:secrets', function (): void {
-    file_put_contents(__DIR__.'/.env', getenv('DOT_ENV'));
-    upload('.env', get('deploy_path').'/shared');
-});
+host('tobexkee.com') // Name of the server
+->setHostname('18.223.113.114') // Hostname or IP address
+->setRemoteUser('forge') // SSH user
+//->stage('production') // Deployment stage (production, staging, etc)
+->setDeployPath('/home/ubuntu/expose');
+
+set('composer_options', '--verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader --ignore-platform-req=php');
+
+after('deploy:failed', 'deploy:unlock'); // Unlock after failed deploy
 
 task('composer:update', function (): void {
     $output = run('sudo composer self-update --2');
     writeln($output);
 });
 
-after('deploy:failed', 'deploy:unlock');
+desc('Deploy the application');
 
-set('composer_options', '--verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader --no-suggest --ignore-platform-reqs');
+// Set up a deployer task to copy secrets to the server.
+// Grabs the dotenv file from the github secret
+task('deploy:secrets', function () {
+    file_put_contents(SOURCE_ROOT . '/.env', getenv('DOT_ENV'));
+    upload(SOURCE_ROOT . '/.env', get('deploy_path') . '/shared');
+});
 
-add('shared_files', []);
-add('shared_dirs', []);
-add('writable_dirs', []);
-
-// Hosts
-host('tobexkee.com')
-    ->setHostname('18.223.113.114')
-    ->setRemoteUser('ubuntu')
-    ->setDeployPath('/home/ubuntu/expose')
-    ->set('labels', ['stage' => 'production']);
-
-// Hooks
-
-/**
- * Main deploy task.
- */
-desc('Deploys your project');
 task('deploy', [
-    'deploy:setup',
     'deploy:info',
     'deploy:lock',
     'deploy:release',
-    'rsync',
-    'deploy:secrets',
+    'rsync', // Deploy code & built assets
+    'deploy:secrets', // Deploy secrets
     'deploy:shared',
     'deploy:vendors',
     'deploy:writable',
-    'artisan:storage:link',
-    'artisan:view:cache',
-    'artisan:config:cache',
-    'artisan:optimize',
-    'artisan:migrate',
-    'artisan:queue:restart',
     'deploy:symlink',
     'deploy:unlock',
     'deploy:cleanup',
 ]);
 
-after('deploy:failed', 'deploy:unlock');
+$deployNginxFiles = function (): void {
+    foreach (DEPLOY_DOMAINS as $localDomain => $DOMAIN) {
+        $linkPath = "/etc/nginx/sites-enabled/$DOMAIN";
+        $filePath = "/etc/nginx/sites-available/$DOMAIN";
+        $command = "
+            sudo cp infrastructure/production/nginx/$localDomain.conf $filePath
+            if [ ! -f \"$linkPath\" ]; then
+                sudo ln -s $filePath $linkPath
+            fi
+        ";
+
+        run($command);
+    }
+};
+
+$deploySupervisorFiles = function (string $stage): void {
+    foreach (SUPERVISOR_CONFIGS as $fileName) {
+        $configPath = "infrastructure/$stage/supervisor/$fileName";
+
+        if (! file_exists($configPath)) {
+            continue;
+        }
+
+        $supervisorPath = "/etc/supervisor/conf.d/$fileName";
+        $content = file_get_contents($configPath);
+        $command = "echo '$content' > $configPath && sudo cp $configPath $supervisorPath";
+
+        run($command);
+    }
+};
+
+$reloadNginx = function (): void {
+    run('sudo nginx -t && sudo service nginx reload');
+};
+
+
+$completeRelease = function () use ($deploySupervisorFiles, $deployNginxFiles, $reloadNginx): void {
+    within('{{release_path}}', function () use ($deploySupervisorFiles, $reloadNginx, $deployNginxFiles): void {
+        run("php expose --version");
+
+        $deploySupervisorFiles('production');
+        $deployNginxFiles();
+        $reloadNginx();
+    });
+
+    run('sudo supervisorctl reread && sudo supervisorctl update && sudo supervisorctl restart expose:*');
+};
+
+task('deploy:done:production', $completeRelease);
+after('deploy', 'deploy:done:production');
